6e1b84da77f0bb100b57686387a7c18a8dfd8e3e
JSXHive <shetkarmalkarsharvari@gmail.com>
2025-11-27 19:50:01 +0530

    Initial commit

---
+++ patterns/pattern23.java
@@ -0,0 +1,14 @@
+package patterns;
+public class pattern23 {
+    public static void main(String[] args) {
+        char ch='A';
+        int n=5;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i+1;j++){
+                System.out.print(ch);
+            }
+            System.out.println();
+            ch++;
+        }
+    }
+}
