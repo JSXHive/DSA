b29b9bef5c3a94ca475007a20420ac86dcfa2089
JSXHive <shetkarmalkarsharvari@gmail.com>
2025-11-27 16:46:46 +0530

    Initial commit

--- pattern5.java
+++ pattern5.java
@@ -2,8 +2,8 @@ public class pattern5 {
     public static void main(String[] args) {
         int n=5;
 
-        for(int i=5;i>=0;i--){
-            for(int j=0;j<i;j++){
+        for(int i=0;i<5;i++){
+            for(int j=5;j>i;j--){
                 System.out.print("* ");
             }
             System.out.println();
